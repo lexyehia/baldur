9fa16fc709af9561fcacc9b1d7158a35a0fcf050
Chad Yehia <chadyehia@sortspoke.com>
2019-07-28 13:30:58 -0400

    Initial

---
+++ Baldur.Server/Baldur.Server/Config/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baldur.Server.Config
+{
+    public class DataContext : DbContext
+    {
+        private readonly string schema;
+
+        public DataContext(string schema)
+        {
+            this.schema = schema;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseNpgsql("postgres://postgres:12345@localhost:5432");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.HasDefaultSchema(schema);
+        }
+    }
+}
---
+++ Baldur.Server/Baldur.Server/Models/Matter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baldur.Server.Models
+{
+    public class Matter
+    {
+    }
+}
